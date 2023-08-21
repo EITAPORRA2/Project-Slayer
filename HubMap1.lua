5229ff120f36145f31328f431d37571dfd2acff1
EITAPORRA2 <135403977+EITAPORRA2@users.noreply.github.com>
2023-08-21 00:26:23 -0300

    Update HubMap1.lua

--- HubMap1.lua
+++ HubMap1.lua
@@ -458,21 +458,6 @@ end)
            end)
        end
     end)
-        
-    spawn(function()
-       while task.wait() do
-          if AutoCollectChest then
-                    for _, v in pairs(game:GetService("Workspace").Debree:GetChildren()) do
-                       if v.Name == "Loot_Chest" then
-                          for _, c in pairs(v:FindFirstChild("Drops"):GetChildren()) do
-                            local args = { [1] = c.Name }
-                                v["Add_To_Inventory"]:InvokeServer(unpack(args))
-                         end
-                     end
-                 end
-            end
-         end
-    end)
     
     spawn(function()
        while task.wait() do
@@ -1250,29 +1235,46 @@ Teleports1:CreateButton({
     end
 })
 
+local Debree = Workspace:WaitForChild("Debree")
+
+local OSTPRKGL = false
 Misc:CreateToggle({
     Name = "AutoCollect",
-    Flag = "AutoCollect",
     CurrentValue = false,
     Callback = function(Value)
-        getgenv().AutoCollectChest = Value
+        OSTPRKGL = Value
+
+        if OSTPRKGL then
+            local connection
+
+            connection = Debree.ChildAdded:Connect(function(v)
+                if v.Name == "Loot_Chest" then 
+                    local Remote = v:WaitForChild("Add_To_Inventory")
+                    local Drops = v:WaitForChild("Drops")  
+                    repeat 
+                        for i,v in next, Drops:GetChildren() do 
+                            Remote:InvokeServer(v.Name)
+                        end
+                        task.wait(0.25)
+                    until not v.Parent
+                end
+            end)
+
+            -- Quando a coleta automática for desativada, desconectamos a função do evento
+            local disconnectSignal
+            disconnectSignal = Misc:CreateToggle({
+                Name = "DisconnectAutoCollect",
+                CurrentValue = false,
+                Callback = function(Value)
+                    if not Value then
+                        connection:Disconnect()
+                        disconnectSignal:Destroy()
+                    end
+                end,
+            })
+        end
     end,
 })
-
-spawn(function()
-    while task.wait() do
-        if getgenv().AutoCollectChest then
-            for _, v in pairs(game:GetService("Workspace").Debree:GetChildren()) do
-                if v.Name == "Loot_Chest" then
-                    for _, c in pairs(v:FindFirstChild("Drops"):GetChildren()) do
-                        local args = { [1] = c.Name }
-                        v["Add_To_Inventory"]:InvokeServer(unpack(args))
-                    end
-                end
-            end
-        end
-    end
-end)
 
 spawn(function()
     while task.wait() do
